4a385f9b5f592232e3767c8ca433b4775db70fe1
qiankeqin <qiankeqin@51xf.cn>
2019-04-04 12:42:46 +0800

    TbResultInfo增加setter/getter/construct

--- src/main/java/com/kj/crosstrade/domain/TbResultInfo.java
+++ src/main/java/com/kj/crosstrade/domain/TbResultInfo.java
@@ -24,4 +24,73 @@ public class TbResultInfo implements Serializable {
     private Date LINE_TIME;
 
     private Integer READ_MARK;
+
+    public TbResultInfo() {
+    }
+
+    public TbResultInfo(String TRADE_CODE, String VOYAGE_NO, String BILL_NO, String ECMDCODE, String LINE_NO, Date LINE_TIME, Integer READ_MARK) {
+        this.TRADE_CODE = TRADE_CODE;
+        this.VOYAGE_NO = VOYAGE_NO;
+        this.BILL_NO = BILL_NO;
+        this.ECMDCODE = ECMDCODE;
+        this.LINE_NO = LINE_NO;
+        this.LINE_TIME = LINE_TIME;
+        this.READ_MARK = READ_MARK;
+    }
+
+    public String getTRADE_CODE() {
+        return TRADE_CODE;
+    }
+
+    public void setTRADE_CODE(String TRADE_CODE) {
+        this.TRADE_CODE = TRADE_CODE;
+    }
+
+    public String getVOYAGE_NO() {
+        return VOYAGE_NO;
+    }
+
+    public void setVOYAGE_NO(String VOYAGE_NO) {
+        this.VOYAGE_NO = VOYAGE_NO;
+    }
+
+    public String getBILL_NO() {
+        return BILL_NO;
+    }
+
+    public void setBILL_NO(String BILL_NO) {
+        this.BILL_NO = BILL_NO;
+    }
+
+    public String getECMDCODE() {
+        return ECMDCODE;
+    }
+
+    public void setECMDCODE(String ECMDCODE) {
+        this.ECMDCODE = ECMDCODE;
+    }
+
+    public String getLINE_NO() {
+        return LINE_NO;
+    }
+
+    public void setLINE_NO(String LINE_NO) {
+        this.LINE_NO = LINE_NO;
+    }
+
+    public Date getLINE_TIME() {
+        return LINE_TIME;
+    }
+
+    public void setLINE_TIME(Date LINE_TIME) {
+        this.LINE_TIME = LINE_TIME;
+    }
+
+    public Integer getREAD_MARK() {
+        return READ_MARK;
+    }
+
+    public void setREAD_MARK(Integer READ_MARK) {
+        this.READ_MARK = READ_MARK;
+    }
 }
